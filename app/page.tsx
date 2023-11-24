47b1136ca747abfd283286b0e8a446316dd15f93
Denis Mytnik <themrcrovvley@gmail.com>
2023-11-24 20:19:24 +0300

    fix: fixes related to url and collection changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HomePage from '@/src/app-pages/HomePage';
 
-const Home = () => <HomePage />;
+// const Home = () => <HomePage />;
+const Home = () => <>хоум пейдж не заполнена</>;
 
 export default Home;

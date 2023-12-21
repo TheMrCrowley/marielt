1e60a19c8fb873c156a54be375c00c9695840a7c
Denis Mytnik <themrcrovvley@gmail.com>
2023-12-21 20:30:58 +0300

    refactor: add try catch block

--- src/services/baseServices.ts
+++ src/services/baseServices.ts
@@ -1,14 +1,19 @@
 const BASE_REVALIDATE_TIME = 60;
 
 export const fetchWrapper = async <T>(url: string, options?: RequestInit): Promise<T> => {
-  const response = await fetch(url, {
-    ...options,
-    next: {
-      revalidate: BASE_REVALIDATE_TIME,
-    },
-  });
+  try {
+    const response = await fetch(url, {
+      ...options,
+      next: {
+        revalidate: BASE_REVALIDATE_TIME,
+      },
+    });
 
-  const data = (await response.json()) as T;
+    const data = (await response.json()) as T;
 
-  return data;
+    return data;
+  } catch (e) {
+    console.error(`Failed to fetch url: ${url}`, (e as Error).message);
+    throw e;
+  }
 };

e93816b625401f52dc46ee4e84704e43ca2aa7b2
Denis Mytnik <themrcrovvley@gmail.com>
2024-01-10 16:24:00 +0300

    feature: add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import { Exo_2 } from 'next/font/google';
+import Script from 'next/script';
 
 import Layout from '@/src/layout';
 
@@ -14,6 +15,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T2WB7LRT');
+        `}
+        </Script>
+      </head>
       <body className={exo_2.className}>
         <Layout>{children}</Layout>
       </body>
